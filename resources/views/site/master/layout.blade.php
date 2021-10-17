68f21bf63c0b37aaa0c1398464c02bb7b340843a
DanilloSB <danillo.ebritto@hotmail.com>
2021-10-17 19:22:02 -0300

    tela login, com email e senha, que salva no firebase Auth

--- resources/views/site/master/layout.blade.php
+++ resources/views/site/master/layout.blade.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="manifest" href="resources/manifest.json">
   
-  <script type="module" src="./configfb.js"></script>
-  <script type="module" src="./funcionalidadefb.js"></script>
+  <script type="Module" src="./configfb.js"></script>
+  <script type="Module" src="./funcionalidadefb.js"></script>
   
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
