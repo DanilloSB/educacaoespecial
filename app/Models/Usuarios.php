d788bab585e598820b8b063c7e543e43794b8966
DanilloSB <danillo.ebritto@hotmail.com>
2021-11-14 19:51:43 -0300

    migration Usuario criada

--- app/Models/Usuarios.php
+++ app/Models/Usuarios.php
@@ -5,12 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Professores extends Model
+class Usuarios extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'nome_do_professor',
-        'nome_da_diciplina',
-    ]
+        'nome', 
+        'cpf', 
+        'telefone', 
+        'nascimento', 
+        'email', 
+        'senha'
+    ];
 }
--- database/migrations/2021_11_14_193218_create_usuarios_table.php
+++ database/migrations/2021_11_14_193218_create_usuarios_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUsuariosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nome');
+            $table->string('cpf')->unique();
+            $table->string('telefone');
+            $table->string('nascimento');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('senha');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuarios');
     }
 }
