51d449f60bfc33248f276a07286cd0af4874c3eb
Sr-Darkpool <atomix.luke@gmail.com>
2021-11-18 20:05:25 -0300

    adicionando diretorio para service worker

--- resources/views/site/master/layout.blade.php
+++ resources/views/site/master/layout.blade.php
@@ -12,6 +12,7 @@
   <script type="Module" src="configfb.js"></script>
   <script src="funcionalidadefb.js"></script>
   
+  
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -51,3 +52,15 @@
 </main>
 </body>
 </html>
+
+<script src="/sw.js" defer></script>
+<script>
+    if (!navigator.serviceWorker.controller) {
+      navigator.serviceWorker.register("/sw.js")
+      .then(function (reg) {
+        console.log("O service worker foi registrado a partir do escopo: " + reg.scope);
+      });["catch"](function (error) {
+    alert('Erro na instalação do Service worker:', error);
+  });
+    }
+</script>
